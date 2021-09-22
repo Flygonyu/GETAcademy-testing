73b4bec9d066698e456b1e49606cbb4c8936bea6
Flygonyu <vanjaschristensen@gmail.com>
2021-09-22 16:01:05 +0200

    buncha changes

--- forApplication/script.js
+++ forApplication/script.js
@@ -1,23 +1,40 @@
+var navBar = document.getElementById('nav');
 var contentDiv = document.getElementById('app');
 
     //model
+    let clicked = '';
+
 
 
     //view
+        
     show();
+    // showExercises();
     function show() {
+        navBar.innerHTML = `<text id="welcome">Welcome!</text>
+        <button class="${clicked}" onclick="exercises()">Exercises</button>
+        <button class="${clicked}" onclick="team()">Team Projects</button>
+        <button class="${clicked}" onclick="about()">About Me</button> 
+        `
+        
         contentDiv.innerHTML = `
         
         
         `
-
-
     }
 
     function showExercises() {
         contentDiv.innerHTML = `
-        Div exercises from module 1!
+        Here are some exercises I've done in module 1:
+        <br><a href="https://flygonyu.github.io/GETAcademy-testing/onclickTest.html" target="_blank">My first onclick test!</a>
+        <br> <a href="https://flygonyu.github.io/Oblig_modul1/Oblig_modul1_VanjaSC.html" target="_blank">Obligatory Exercise</a>
+        <br> <a href="" target="_blank">Placeholder</a>
+        <br> <a href="" target="_blank">Placeholder</a>
+        <br> <a href="" target="_blank">Placeholder</a>
+        <br> <a href="" target="_blank">Placeholder</a>
+        <br> <a href="" target="_blank">Placeholder</a>
         `
+        
     }
 
     function showTeam() {
@@ -34,7 +51,8 @@ var contentDiv = document.getElementById('app');
 
     //controller
     function exercises(){
-
+    clicked = 'clicked';
+    show();
     showExercises()
     }
 
