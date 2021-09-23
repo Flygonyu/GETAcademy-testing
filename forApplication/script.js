cfb741694b8940b6ba210ed0ba71bcc507345d0f
Flygonyu <vanjaschristensen@gmail.com>
2021-09-23 13:21:57 +0200

    started grid++

--- forApplication/script.js
+++ forApplication/script.js
@@ -2,19 +2,19 @@ var navBar = document.getElementById('nav');
 var contentDiv = document.getElementById('app');
 
     //model
-    let clicked = '';
 
 
 
     //view
-        
     show();
-    // showExercises();
+    showAbout();
+
     function show() {
         navBar.innerHTML = `<text id="welcome">Welcome!</text>
         <button class="" onclick="exercises()">Exercises</button>
         <button class="" onclick="team()">Team Projects</button>
         <button class="" onclick="about()">About Me</button> 
+        <a href="https://github.com/Flygonyu" target="_blank"><img id="gitLogo" src="Octicons-mark-github.svg"></a>
         `
         
         contentDiv.innerHTML = `
@@ -28,19 +28,22 @@ var contentDiv = document.getElementById('app');
         <button class="clicked" onclick="exercises()">Exercises</button>
         <button class="" onclick="team()">Team Projects</button>
         <button class="" onclick="about()">About Me</button> 
+        <a href="https://github.com/Flygonyu" target="_blank"><img id="gitLogo" src="Octicons-mark-github.svg"></a>
         `
         contentDiv.innerHTML = `
-        Here are some exercises I've done in module 1:
+        <h2>Here are some exercises I've done in module 1:</h2>
+        <div id="exer">
         <br><a href="https://flygonyu.github.io/GETAcademy-testing/onclickTest.html" target="_blank">My first onclick test!</a>
-        <br> <a href="https://flygonyu.github.io/Oblig_modul1/Oblig_modul1_VanjaSC.html" target="_blank">Obligatory Exercise</a>
         <br> <a href="https://flygonyu.github.io/GETAcademy-testing/dynamiskHTMLferdighet6.html" target="_blank">Dynamic HTML</a>
         <br> <a href="https://flygonyu.github.io/GETAcademy-testing/variabelRegnestykke_ferdighet7.html" target="_blank">Math-y Variables</a>
         <br> <a href="https://flygonyu.github.io/GETAcademy-testing/kodel%C3%A5s.html" target="_blank">Pin Code (wip)</a>
         <br> <a href="https://flygonyu.github.io/GETAcademy-testing/ludo.html" target="_blank">SVG Ludo</a>
+        <br> <a href="https://flygonyu.github.io/Oblig_modul1/Oblig_modul1_VanjaSC.html" target="_blank">Obligatory Exercise</a>
         <br> <a href="" target="_blank">Placeholder</a>
         <br> <a href="" target="_blank">Placeholder</a>
         <br> <a href="" target="_blank">Placeholder</a>
         <br> <a href="" target="_blank">Placeholder</a>
+        </div>
         `
         
     }
@@ -50,14 +53,17 @@ var contentDiv = document.getElementById('app');
         <button class="" onclick="exercises()">Exercises</button>
         <button class="clicked" onclick="team()">Team Projects</button>
         <button class="" onclick="about()">About Me</button> 
+        <a href="https://github.com/Flygonyu" target="_blank"><img id="gitLogo" src="Octicons-mark-github.svg"></a>
         `
         contentDiv.innerHTML = `
-        Our team's projects from module 1:
+        <h2>Our team's projects from module 1:</h2>
+        <div id="team">
         <br> <a href="https://flygonyu.github.io/team2/Tic-tac-bigtoe.html" target="_blank">Tic-Tac-Toe</a>
         <br> <a href="https://flygonyu.github.io/Teamoppgave_uke3_team2/puslespill/index.html" target="_blank">Jigsaw Puzzle</a>
         <br> <a href="https://flygonyu.github.io/historie-team2/index.html" target="_blank">Wordchooser Story</a>
         <br> <a href="https://flygonyu.github.io/SteinSaksPapir_team2/index.html" target="_blank">Rock-Paper-Scissors</a>
         <br> <a href="https://flygonyu.github.io/morsecode/index.html" target="_blank">Morse Code Translator</a>
+        </div>
         `
     }
 
@@ -66,27 +72,37 @@ var contentDiv = document.getElementById('app');
         <button class="" onclick="exercises()">Exercises</button>
         <button class="" onclick="team()">Team Projects</button>
         <button class="clicked" onclick="about()">About Me</button> 
+        <a href="https://github.com/Flygonyu" target="_blank"><img id="gitLogo" src="Octicons-mark-github.svg"></a>
         `
         contentDiv.innerHTML = `
-        Hi! I'm Vanja. 25 yrs old from Sandefjord, NO.
+        <div id="aboutWrapper">
+        <h2 class="header">Hello! I'm Vanja.</h2> 
+        <div class="intro" id="bio">
+        <p>I'm 25, and I live in Sandefjord, Norway.</p>
+        My hobbies include reading, gaming, and Dungeons & Dragons.
+        </div>
+        <img id="me" class="picture" src="profilepic.jpg">
+        <div class="game"><h3>Gaming</h3>
+        I mostly play indie games and RPGs. I love getting all achievements in games, and I'll spend hours making sure I've completed everything. I love lists, and I'll sometimes plan my playthroughs beforehand, trying to find a way I can complete everything faster.</div>
+        </div>
         `
     }
 
     //controller
     function exercises(){
-    clicked = 'clicked';
+    
     show();
     showExercises()
     }
 
     function team() {
-        clicked = 'clicked';
+        
         show();
         showTeam();
     }
 
     function about(){
-        clicked = 'clicked';
+        
         show();
         showAbout();
     }
